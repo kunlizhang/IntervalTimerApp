6c0e987dc203683d3b868f4332c0b5a6c2ff1659
Kunli Zhang <kunlizhang7@gmail.com>
2022-01-20 23:26:36 -0500

    Initial Commit

---
+++ Shared/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  Shared
+//
+//  Created by Kunli Zhang on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
